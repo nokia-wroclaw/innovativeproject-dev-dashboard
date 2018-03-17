edc1201f37c8127f8514e68812fc3e31b37ac17a
Bartosz Pogoda <bartek.pogoda1@gmail.com>
2018-03-17 01:07:50 +0100

    displaying panel id route param for panel configuration component

--- src/Dashboard.WebApi/developersDashboardFrontEnd/src/app/panel-configuration/panel-configuration.component.ts
+++ src/Dashboard.WebApi/developersDashboardFrontEnd/src/app/panel-configuration/panel-configuration.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
-@Component({
-  selector: 'app-panel-configuration',
-  templateUrl: './panel-configuration.component.html',
-  styleUrls: ['./panel-configuration.component.css']
-})
-export class PanelConfigurationComponent implements OnInit {
+@Component({selector: 'app-panel-configuration', templateUrl: './panel-configuration.component.html', styleUrls: ['./panel-configuration.component.css']})
+export class PanelConfigurationComponent implements OnInit,
+OnDestroy {
 
-  constructor() { }
+  constructor(private route : ActivatedRoute) {}
+
+  panelId : number;
+
+  private routeParamsSubscription;
 
   ngOnInit() {
+    this.routeParamsSubscription = this
+      .route
+      .params
+      .subscribe(params => {
+        this.panelId = params['id'];
+      });
   }
 
+  ngOnDestroy() {
+    this
+      .routeParamsSubscription
+      .unsubscribe();
+  }
 }
