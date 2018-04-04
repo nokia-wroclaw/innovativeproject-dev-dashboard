a291e94d3b983d037431b61ad366bd96314ad72f
leereek@gmail.com <leereek@gmail.com>
2018-04-04 05:11:30 +0200

    Working squashed Travis deployment to heroku

--- src/Dashboard.WebApi/developersDashboardFrontEnd/nginx/serve.sh
+++ src/Dashboard.WebApi/developersDashboardFrontEnd/nginx/serve.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+if [ -z "$PORT" ]; then
+    echo "serve.sh > Need to set PORT"
+    exit 1
+fi  
+if [ -z "$API_HOST" ]; then
+    echo "serve.sh > Need to set API_HOST"
+    exit 1
+fi 
+
+echo replacing EXTERN_PORT/$PORT
+echo replacing EXTERN_API_HOST/$API_HOST
+
 sed -i "s/EXTERN_PORT/$PORT/g" /etc/nginx/conf.d/default.conf
-echo /etc/nginx/conf.d/default.conf
+sed -i "s/EXTERN_API_HOST/$API_HOST/g" /etc/nginx/conf.d/default.conf
+
+echo "starting daemon"
 nginx -g "daemon off;"
