acd1afa450f27a3b0019b890c7d030f169c2efa4
RandomName <randommail729@gmail.com>
2018-04-14 13:22:11 +0200

    working on new responsive design of pipeline visualization [WIP]

--- src/Dashboard.WebApi/developersDashboardFrontEnd/src/app/panels/shared/pipeline-view/pipeline-view.component.ts
+++ src/Dashboard.WebApi/developersDashboardFrontEnd/src/app/panels/shared/pipeline-view/pipeline-view.component.ts
@@ -1,48 +1,58 @@
-import { Component, OnInit, Input, ElementRef } from '@angular/core';
-import { Pipeline, Stage } from '../../../projects-manager/project';
+import {Component, OnInit, Input, ElementRef, ViewChild} from '@angular/core';
+import {Pipeline, Stage} from '../../../projects-manager/project';
 
-@Component({
-  selector: 'app-pipeline-view',
-  templateUrl: './pipeline-view.component.html',
-  styleUrls: ['./pipeline-view.component.css']
-})
+@Component({selector: 'app-pipeline-view', templateUrl: './pipeline-view.component.html', styleUrls: ['./pipeline-view.component.css']})
 export class PipelineViewComponent implements OnInit {
 
   @Input()
   pipeline : Pipeline;
 
-  ngOnInit() {
-    console.log("Pipeline view for: " + this.pipeline)
-  }
+  @ViewChild('pipelineContainer') 
+  pipelineContainer : ElementRef; 
 
-  statusColors: any[] = [{
-    status: "failed",
-    color: '#d9534f'
-  }, {
-    status: "running",
-    color: 'blue'
-  }, {
-    status: "manual",
-    color: 'pink'
-  }, {
-    status: "success",
-    color: 'green'
-  }, {
-    status: "created",
-    color: "white"
-  }, {
-    status: "skipped",
-    color: "gray"
-  }];
+  pipelineContainerHeight : number = 70;
 
-  constructor(private elementRef: ElementRef) {
+  ngOnInit() { }
+
+  statusColors : any[] = [
+    {
+      status: "failed",
+      color: '#d9534f'
+    }, {
+      status: "running",
+      color: 'blue'
+    }, {
+      status: "manual",
+      color: 'pink'
+    }, {
+      status: "success",
+      color: 'green'
+    }, {
+      status: "created",
+      color: "white"
+    }, {
+      status: "skipped",
+      color: "gray"
+    }
+  ];
+
+  constructor(private elementRef : ElementRef) {
     console.log(elementRef);
+
     // TODO scale visualisation based on actual dimensions of component
-    //elementRef.nativeElement.clientWidth;
+    // console.log('Client width: ' + elementRef.nativeElement.clientWidth);
   }
 
-  getColorOfStage(stage: Stage) {
-    return this.statusColors.find(statusColor => statusColor.status == stage.stageStatus).color;
+  getColorOfStage(stage : Stage) {
+
+    return this
+      .statusColors
+      .find(statusColor => statusColor.status == stage.stageStatus)
+      .color;
+  }
+
+  getOneStageWidth() : number {
+    return this.pipelineContainer.nativeElement.clientWidth / this.pipeline.stages.length;
   }
 
 }
