f992d93872df2dc3cf22371e967b4aaecd5e30d3
SebastianSzkoluda <sebastianszkoluda@gmail.com>
2018-04-18 11:29:39 +0200

    error and success types on snackbar

--- src/Dashboard.WebApi/developersDashboardFrontEnd/src/app/configuration/projects-list/projects-list.component.ts
+++ src/Dashboard.WebApi/developersDashboardFrontEnd/src/app/configuration/projects-list/projects-list.component.ts
@@ -17,28 +17,28 @@ export class ProjectsListComponent implements OnInit {
     responsiveView: true,
     resizable: false,
     useCSSTransforms: true,
-    widthHeightRatio: 1.3,
+    widthHeightRatio: 1.9,
     responsiveOptions: [
       {
         breakpoint: 'sm',
-        lanes: 3
+        minWidth: 1024,
+        lanes: 3,
+        widthHeightRatio: 1.4,
       }, {
         breakpoint: 'md',
-        minWidth: 768,
-        lanes: 4,
-        dragAndDrop: true,
-        resizable: true
+        minWidth: 1280,
+        widthHeightRatio: 1.4,
+        lanes: 4 ,
       }, {
         breakpoint: 'lg',
-        lanes: 6,
-        dragAndDrop: true,
-        resizable: true
+        minWidth: 1400,
+        lanes: 5,
+        widthHeightRatio: 1.4,
       }, {
         breakpoint: 'xl',
         minWidth: 1800,
-        lanes: 8,
-        dragAndDrop: true,
-        resizable: true
+        lanes: 6,
+        widthHeightRatio: 1.3,
       }
     ]
   };
