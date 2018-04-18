410c1f2bce3ae48e0517a723094f5889ef65d443
leereek@gmail.com <leereek@gmail.com>
2018-04-18 17:46:57 +0200

    Travis cache + ProjectId fix

--- src/Dashboard.Core/Entities/Panel.cs
+++ src/Dashboard.Core/Entities/Panel.cs
@@ -12,7 +12,7 @@ namespace Dashboard.Core.Entities
 
         public abstract string Discriminator { get; }
 
-        public int? ProjectId { get; private set; }
+        public int? ProjectId { get; set; }
         public virtual Project Project { get; set; }
 
         public bool ShouldSerializeProject()
