adc03cb98eb066a808a6dfa41a4fb24965d8fe36
bartoszpogoda <bartek.pogoda1@gmail.com>
2018-06-01 18:06:05 +0200

    implemented no title-bar mode

--- src/Dashboard.WebApi/developersDashboardFrontEnd/src/app/configuration/dashboard-configuration/dashboard-service/dashboard-configuration.service.ts
+++ src/Dashboard.WebApi/developersDashboardFrontEnd/src/app/configuration/dashboard-configuration/dashboard-service/dashboard-configuration.service.ts
@@ -1,12 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class DashboardConfigurationService {
 
-  hidePanelTitle: boolean = false;
-  
 
+  hidePanelTitle: boolean = false;
+
+  hideTopBar:  boolean = false;
+
+  private subjectHideTopBar: Subject<boolean> = new Subject();
+  
   constructor() { }
+
+  setHideTopBar(hideTopBar: boolean) {
+    this.hideTopBar = hideTopBar;
+    this.subjectHideTopBar.next(hideTopBar);
+  }
+
+  getHideTopBarSubject() : Subject<boolean> {
+    return this.subjectHideTopBar;
+  }
+
+  getHideTopBar(): boolean {
+    return this.hideTopBar;
+  }
 
   setPanelTitleStatus(panelTitleStatus: boolean){
     this.hidePanelTitle = panelTitleStatus;
